9f94313b331abb442078aff01c42c49d2a420040
migu4917 <491745115@qq.com>
2022-04-28 16:00:24 +0800

    unittests: add simple unittests frame

--- server/demo/urls.py
+++ server/demo/urls.py
@@ -7,28 +7,50 @@ urlpatterns = [
     path("api/upload", upload.upload, name="upload"),
 
     path("api/query", query.query, name="query"),
-    path("api/queryEpidemic", query.queryEpidemic),  # 查询某个疫情数据
-    path("api/queryEpidemicPerson", query.queryEpidemicPerson),  # 查询属于该波疫情的所有人员的资料
+    path("api/queryEpidemic", query.queryEpidemic,
+         name="queryEpidemic"),  # 查询某个疫情数据
+
+    path("api/queryEpidemicPerson", query.queryEpidemicPerson,
+         name="queryEpidemicPerson"),  # 查询属于该波疫情的所有人员的资料
+
     # 查询这个范围内的所有detail_location
-    path("api/queryDetailLocation", query.queryDetailLocation),
-    path("api/queryperson", query.queryperson),
+    path("api/queryDetailLocation", query.queryDetailLocation,
+         name="queryDetailLocation"),
 
-    path("api/insertroute", route.insertRoute),
+    path("api/queryperson", query.queryperson, name="queryperson"),
 
-    path("api/queryRelatedInfo", related.queryRelatedInfo),
+    path("api/insertroute", route.insertRoute, name="insertroute"),
 
-    path("api/getall", views.getall),
-    path("api/testget", views.testget),
-    path("api/newupload", views.newupload),
-    path("api/update", views.update),
-    path("api/delete", views.deleteperson),
-    path("api/epidemicUpload", views.epidemicUpload),  # 上传疫情数据
-    path("api/contactUpload", views.contactUpload),  # 上传密接者数据
-    path("api/getAllContacts", views.getAllContacts),  # 得到所有密接者数据
-    path("api/getAllEpidemics", views.getAllEpidemics),  # 得到所有疫情数据
-    path("api/updateNull", views.updateNull),
-    path("api/getAllPlace", views.getAllPlace),
-    path("api/initData", views.initData),
+    path("api/queryRelatedInfo", related.queryRelatedInfo, name="queryRelatedInfo"),
+
+    path("api/getall", views.getall, name="getall"),
+
+    path("api/testget", views.testget, name="testget"),
+
+    path("api/newupload", views.newupload, name="newupload"),
+
+    path("api/update", views.update, name="update"),
+
+    path("api/delete", views.deleteperson, name="delete"),
+
+    path("api/epidemicUpload", views.epidemicUpload,
+         name="epidemicUpload"),  # 上传疫情数据
+
+    path("api/contactUpload", views.contactUpload,
+         name="contactUpload"),  # 上传密接者数据
+
+    path("api/getAllContacts", views.getAllContacts,
+         name="getAllContacts"),  # 得到所有密接者数据
+
+    path("api/getAllEpidemics", views.getAllEpidemics,
+         name="getAllEpidemics"),  # 得到所有疫情数据
+
+    path("api/updateNull", views.updateNull, name="updateNull"),
+
+    path("api/getAllPlace", views.getAllPlace, name="getAllPlace"),
+
+    path("api/initData", views.initData, name="initData"),
+
     # path("api/text2Info", views.text2Info),
     # path("api/download", views.download_template),
     # path("api/downloadDocx", views.download_docx),
